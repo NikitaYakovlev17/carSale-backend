c63a20e41aea113dd0ea2661aef27c053d0d0873
NikitaYakovlev17 <nikita2001pmc17@mail.ru>
2022-04-27 01:11:04 +0300

    some changes in DB

--- src/main/java/com/yakovlev/car/sale/controller/CarAdController.java
+++ src/main/java/com/yakovlev/car/sale/controller/CarAdController.java
@@ -13,6 +13,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -88,9 +89,11 @@ public class CarAdController {
         var outputStream = dropboxService.getFile("/carAdPhotos/" + path);
         byte[] bytes = outputStream.toByteArray();
         InputStream inputStream = new ByteArrayInputStream(bytes);
+        String extension = path.split("\\.")[1];
 
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + path + "\"")
+                .contentType(MediaType.parseMediaType("image/" + extension))
+                //.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + path + "\"")
                 .body(new InputStreamResource(inputStream));
     }
 }
